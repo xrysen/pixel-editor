5c92a688f67043349c224fbfd092da26020fc09f
Sean Oyler <xrysen@gmail.com>
2021-01-25 19:48:02 -0800

    Adds right-click erase pixel

--- draw.js
+++ draw.js
@@ -1,3 +1,4 @@
+
 const makeGrid = () => {
   for (let i = 0; i < 32; i++) {
     $(".canvas").append(`<tr id=tr${i}></tr>`);
@@ -14,10 +15,15 @@ makeGrid();
 
 for (let i = 0; i < 32; i++) {
   for (let j = 0; j < 32; j++) {
-    $(`#${i}-${j}`).on('mousedown', () => {
-      $(`#${i}-${j}`).css("background-color", $("#colour-picker").val());
+    $(`#${i}-${j}`).on('mousedown', (e) => {
+      switch(e.which) {
+        case 1:
+        $(`#${i}-${j}`).css("background-color", $("#colour-picker").val());
+        break;
+        case 3:
+        $(`#${i}-${j}`).css("background-color", "white"); 
+        break;
+      }
     })
   }
 }
-
-console.log($("#colour-picker"));
