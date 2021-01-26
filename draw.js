526ab7ff5568bd99a94e52608ece69ce6118cf53
Sean Oyler <xrysen@gmail.com>
2021-01-25 18:20:01 -0800

    Will fill in square on click based on colour in colour-picker

--- draw.js
+++ draw.js
@@ -5,9 +5,19 @@ const makeGrid = () => {
 
   for (let i = 0; i < 32; i++) {
     for (let j = 0; j < 32; j++) {
-      $(`#tr${i}`).append("<td></td>");
+      $(`#tr${i}`).append(`<td id=${i}-${j}></td>`);
     }
   }
 }
 
 makeGrid();
+
+for (let i = 0; i < 32; i++) {
+  for (let j = 0; j < 32; j++) {
+    $(`#${i}-${j}`).on('mousedown', () => {
+      $(`#${i}-${j}`).css("background-color", $("#colour-picker").val());
+    })
+  }
+}
+
+console.log($("#colour-picker"));
