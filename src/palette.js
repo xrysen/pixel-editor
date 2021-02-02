d2ac39b5f4fdf1cadf274f62b850a03a6ef77bb9
Sean Oyler <xrysen@gmail.com>
2021-02-02 13:59:51 -0800

    Adds ability to choose colour from selected palette

--- src/palette.js
+++ src/palette.js
@@ -1,6 +1,8 @@
 const drawPaletteBackground = () => {
   for (let i = 0; i < 16; i++) {
-    $("#palette").append(`<div id = "colour-${i}" class = 'colour-selection'></div>`);
+    $("#palette").append(
+      `<div id = "colour-${i}" class = 'colour-selection'></div>`
+    );
   }
 };
 
@@ -9,8 +11,8 @@ const generatePalette = (palette) => {
     $(`#colour-${i}`).css("background-color", palette[i]);
     console.log(palette[i]);
   }
-  $(`#colour-15`).append("<div class = 'colour-selector'></div>");
-}
+  $(`#colour-0`).append("<div class = 'colour-selector'></div>");
+};
 
 drawPaletteBackground();
 generatePalette(palettes[0]);
@@ -18,6 +20,13 @@ generatePalette(palettes[0]);
 for (let i = 0; i < 16; i++) {
   $(`#colour-${i}`).on("click", () => {
     $(".colour-selector").remove();
-    $(`#colour-${i}`).append("<div class = 'colour-selector'></div>");   
-  })
+    $(`#colour-${i}`).append("<div class = 'colour-selector'></div>");
+    selectedColour = getRGBValues($(`#colour-${i}`).css("background-color"));
+    selectedColour = convertRGBtoHex(
+      Number(selectedColour[0]),
+      Number(selectedColour[1]),
+      Number(selectedColour[2])
+    );
+    console.log(selectedColour);
+  });
 }
