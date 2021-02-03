46b12f93361dc95bb11dfd35c6d374d48ec0f474
Sean Oyler <xrysen@gmail.com>
2021-02-03 15:28:28 -0800

    Generates and shows all palettes

--- src/js/palette.js
+++ src/js/palette.js
@@ -13,8 +13,29 @@ const generatePalette = (palette) => {
   $(`#colour-0`).append("<div class = 'colour-selector'></div>");
 };
 
+const showAllPalettes = () => {
+  for (let i = 0; i < palettes.length; i++) {
+    $(`#choose-palette`).append(`<div id = "palette-${i}" class="palette-container"></div>`);
+    for (let j = 0; j < palettes[i].length; j++) {
+      $(`#palette-${i}`).append(`<div id = "colours-${i}-${j}" class = 'colour-selection'></div>`);
+      
+    }
+  }
+}
+
+const showAll = () => {
+  showAllPalettes();
+  for (let i = 0; i < palettes.length; i++) {
+    for (let j = 0; j < palettes[i].length; j++) {
+      console.log(palettes[i][j]);
+      $(`#colours-${i}-${j}`).css("background-color", palettes[i][j]);
+    }
+  }
+}
+
 drawPaletteBackground();
 generatePalette(palettes[0]);
+showAll();
 
 for (let i = 0; i < 16; i++) {
   $(`#colour-${i}`).on("click", () => {
@@ -29,3 +50,4 @@ for (let i = 0; i < 16; i++) {
     console.log(selectedColour);
   });
 }
+
